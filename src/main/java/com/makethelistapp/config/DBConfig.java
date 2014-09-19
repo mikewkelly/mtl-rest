282de50abce70de64d9640ab885e699c6dcb084e
Mike Kelly <mikewkelly@hotmail.com>
2014-09-19 13:57:03 -0700

    Added DBConfig class

---
+++ src/main/java/com/makethelistapp/config/DBConfig.java
@@ -0,0 +1,9 @@
+package com.makethelistapp.config;
+
+public class DBConfig {
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String AUTH_DB_URL = "jdbc:mysql://localhost/mtl";
+	static final String DB_USER = "root";
+	static final String DB_PASS = "root";
+
+}
