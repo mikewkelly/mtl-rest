82e8462d67e2200ca7a2c59a240ee0297c6ff130
Mike Kelly <mikewkelly@hotmail.com>
2014-09-19 13:45:51 -0700

    Added base config classes

---
+++ src/main/java/com/makethelistapp/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.makethelistapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+//TODO: Change this to check varying usernames and passwords
+
+  @Override
+  protected void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("admin").password("admin").roles("USER");
+  }
+  
+  //TODO: change this to match API URI structure
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeUrls()
+        .antMatchers("/aggregators/**").hasRole("USER")
+        .anyRequest().anonymous()
+        .and()
+        .httpBasic();
+  }
+}
