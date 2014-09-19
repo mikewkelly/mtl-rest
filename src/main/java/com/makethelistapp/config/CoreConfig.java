0cf4934501c18a49acfc80343a5f95515e04c7c2
Mike Kelly <mikewkelly@hotmail.com>
2014-09-19 15:13:16 -0700

    No update

--- src/main/java/com/makethelistapp/config/CoreConfig.java
+++ src/main/java/com/makethelistapp/config/CoreConfig.java
@@ -1,10 +1,8 @@
 package com.makethelistapp.config;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.UUID;
 
 @Configuration
 public class CoreConfig {
